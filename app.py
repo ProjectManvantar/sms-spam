36b4bc41f2827ca00b5ee9fe418e29e836e19b07
Konda Ramakanth Reddy <99175525+ProjectManvantar@users.noreply.github.com>
2024-04-02 20:33:12 +0545

    Update app.py

--- app.py
+++ app.py
@@ -56,12 +56,12 @@ background-size: cover;
 st.title("SMS Spam Detection Model")
 st.subheader('"SMS spam is a growing problem, but with the help of machine learning, we can fight back!"')
 
-# Add an About Us section on the left side of the web page
-col1, col2 = st.beta_columns([1, 3])
+# Add an "About Us" section on the left side of the web page
+col1, col2 = st.beta_columns(2)
 with col1:
     st.subheader("About Us")
-    st.write("We are a team of passionate data scientists and engineers dedicated to fighting SMS spam. Our model uses advanced machine learning techniques to accurately classify SMS messages as spam or not spam.")
-    st.write('"We believe that technology can be used to make the world a better place, one SMS at a time."')
+    st.write("We are a team of passionate individuals dedicated to fighting SMS spam. Our goal is to provide an easy-to-use and effective solution for detecting and filtering out unwanted SMS messages.")
+    st.write("Our model is trained on a large dataset of SMS messages and uses advanced machine learning techniques to accurately classify messages as spam or not spam.")
 
 # Add an input box for the SMS
 input_sms = st.text_input("Enter the SMS", help="Type your SMS here...", max_chars=500)
@@ -83,4 +83,3 @@ if st.button('Predict', key='predict'):
             st.success("Not Spam")
     else:
         st.warning("Please enter an SMS.")
-
