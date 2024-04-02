81fb18360550a643c7370a89694d49364e33401b
Konda Ramakanth Reddy <99175525+ProjectManvantar@users.noreply.github.com>
2024-04-02 19:45:21 +0545

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,12 @@ nltk.download('punkt')
 nltk.download('stopwords')
 
 import streamlit as st
-import pickle 
+import pickle
 import string
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
-
 
 def transform_text(text):
     text = text.lower()
@@ -35,26 +34,69 @@ def transform_text(text):
 
     return " ".join(y)
 
-
 tk = pickle.load(open("vectorizer.pkl", 'rb'))
 model = pickle.load(open("model.pkl", 'rb'))
 
-st.title("SMS Spam Detection Model")
-st.write("*Great to see you here !!!*")
-    
+# Add a background image
+st.set_page_config(page_title="SMS Spam Detection Model", page_icon=":envelope:", layout="wide", initial_sidebar_state="expanded")
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url("https://wallpaperaccess.com/full/2819254.jpg");
+        background-attachment: fixed;
+        background-size: cover
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
-input_sms = st.text_input("Enter the SMS")
+col1, col2 = st.columns(2)
 
-if st.button('Predict'):
+with col1:
+    st.title("SMS Spam Detection Model")
+    st.write("*Great to See YOU Here!!!*")
+    st.write("This app predicts whether an SMS is spam or not based on the text of the message.")
 
-    # 1. preprocess
-    transformed_sms = transform_text(input_sms)
-    # 2. vectorize
-    vector_input = tk.transform([transformed_sms])
-    # 3. predict
-    result = model.predict(vector_input)[0]
+with col2:
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("https://wallpaperaccess.com/full/2819254.jpg");
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: contain;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+input_sms = st.text_input("Enter the SMS", placeholder="Type your SMS here...", key="input")
+
+if st.button('Predict', key="predict"):
+    with st.spinner('Processing...'):
+        # 1. preprocess
+        transformed_sms = transform_text(input_sms)
+        st.write("**Processed Text:**", transformed_sms)
+
+        # 2. vectorize
+        vector_input = tk.transform([transformed_sms])
+
+        # 3. predict
+        result = model.predict(vector_input)[0]
+
     # 4. Display
     if result == 1:
         st.header("Spam")
+        st.write("The SMS you entered is predicted to be spam.")
     else:
         st.header("Not Spam")
+        st.write("The SMS you entered is predicted to be not spam.")
+
+    # Add a feedback mechanism
+    user_feedback = st.selectbox("Was the prediction correct?", ["Yes", "No"])
+    if user_feedback:
+        st.write(f"Thanks for your feedback! You said the prediction was {user_feedback}.")
